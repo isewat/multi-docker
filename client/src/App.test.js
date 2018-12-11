b6cb1b130771893c5c05aabad90a954170b62e1a
Ralf <isewat@gmail.com>
2018-12-11 23:36:17 +0100

    initial setup

---
+++ client/src/App.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+  // Don't test this because of dependencies to Redis & Postgres
+  // TODO: set up faker module to mock JSON data to render
+});
